d8146610845235d7557dbd4562e497f73d7638aa
Jaider <jaider.grico@gmail.com>
2026-03-30 13:12:05 -0500

    implement ISponsorRepository and SponsorRepository with specific search methods

---
+++ SportsLeague/SportsLeague.API/DTOs/Request/SponsorRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.API.DTOs.Request;
+
+public class SponsorRequestDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public string Industry { get; set; } = string.Empty;
+    public string ContactEmail { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public SponsorCategory Category { get; set; }
+}
---
+++ SportsLeague/SportsLeague.API/DTOs/Response/SponsorResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsLeague.API.DTOs.Response;
+
+public class SponsorResponseDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Industry { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty; // Aquí mapearemos el string del Enum
+}
